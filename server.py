4f0d839a93f957147b14f278a048cb4af35de54a
kaliv <85838942+Friedrich-M@users.noreply.github.com>
2022-05-24 20:08:57 +0800

    RSA解密

--- server.py
+++ server.py
@@ -5,15 +5,47 @@ from array import array
 from math import ceil
 from struct import unpack
 import numpy as np
+import rsa
+import pickle
 
+PORT = 502
+BUFF = 1024
+
+
+def RsaEncrypt(strArray):
+    (PubKey, PrivateKey) = rsa.newkeys(128)
+    Encrypt_Str_Array = []
+    for data in strArray:
+        content = str(data).encode('utf8')
+        Encrypt_Str = rsa.encrypt(content, PubKey)
+        Encrypt_Str_Array.append(Encrypt_Str)
+    return (Encrypt_Str_Array, PrivateKey)
+
+
+# rsa decryption process
+def RsaDecrypt(strArray, pk):
+    Decrypt_Str_Array = []
+    for data in strArray:
+        content = rsa.decrypt(data, pk)
+        content = content.decode('utf8')
+        # 转换为unsigned int
+        content = int(content)
+        Decrypt_Str_Array.append(content)
+    return array('B', Decrypt_Str_Array)
 
 def TCP(conn, addr):
-	buffer = array('B', [0] * 300)
 	cnt = 0
 	hexOf = lambda BUFFER: ','.join([hex(i) for i in BUFFER])
 	while True:
 		try:
-			conn.recv_into(buffer)
+			Message = conn.recv(BUFF)
+			# Detects if the received data is empty
+			if Message:
+				(recvdata, PrivateKey) = pickle.loads(Message)
+			else:
+				return 
+			buffer = RsaDecrypt(recvdata, PrivateKey)
+
 			TID0 = buffer[0]   #Transaction ID	to sync
 			TID1 = buffer[1]   #Transaction ID 
 			ID = buffer[6]	   #Unit ID
@@ -38,12 +70,23 @@ def TCP(conn, addr):
 				else:
 					DAT = array('B', np.arange(cnt, LEN+cnt, dtype=np.dtype('>i2')).tobytes())
 					DATA = [i for i in range(cnt, LEN+cnt)] 
-				conn.send(array('B', [TID0, TID1, 0, 0, 0, BYT + 3, ID, FC, BYT]) + DAT)
+ 
+				cmd = array('B', [TID0, TID1, 0, 0, 0, BYT + 3, ID, FC, BYT]) + DAT
+				(encryptdata, PrivateKey) = RsaEncrypt(cmd)
+				Message = pickle.dumps([encryptdata, PrivateKey]) 
+				conn.send(Message) 
+
+				# conn.send(array('B', [TID0, TID1, 0, 0, 0, BYT + 3, ID, FC, BYT]) + DAT)
 				print(f"TID = {(TID0 * 256 + TID1)}, ID= {ID}, Fun.Code= {FC}, Address= {ADR}, Length= {LEN}, Data= {DATA}\n--------")
+
 				if cnt < 60000: cnt = cnt + 1
 				else: cnt = 1
 			elif (FC in [5, 6, 15, 16]):  # Write Registers
-				conn.send(array('B', [TID0, TID1, 0, 0, 0, 6, ID, FC, mADR, lADR, buffer[10], buffer[11]]))
+				cmd = array('B', [TID0, TID1, 0, 0, 0, 6, ID, FC, mADR, lADR, buffer[10], buffer[11]])
+				(encryptdata, PrivateKey) = RsaEncrypt(cmd)
+				Message = pickle.dumps([encryptdata, PrivateKey]) 
+				conn.send(Message) 
+				
 				if FC in [5,6]:
 					BYT = 2
 					buf = buffer[10:12]
@@ -64,10 +107,12 @@ def TCP(conn, addr):
 			exit()
 
 if __name__ == '__main__':
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind(('', 502))
-	s.listen(1)
+	ServerSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	ServerSock.bind(('',PORT))
+	ServerSock.listen(1)
+	print("listening......")
 	while True:
-		conn, addr = s.accept()
-		print("Connected by", addr[0])
-		_thread.start_new_thread(TCP, (conn, addr))
+		ConSock,addr = ServerSock.accept()
+		print(f'Connected successfully by {addr[0]}' + '\n')
+		_thread.start_new_thread(TCP, (ConSock, addr))
+
